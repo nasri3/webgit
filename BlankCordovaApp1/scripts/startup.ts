25efa522d6918669f3b8a56b3e3a9393eaad681e
nasri <nasri@DESKTOP-UVG3151>
2018-03-18 08:48:31 +0100

    import2

---
+++ BlankCordovaApp1/scripts/startup.ts
@@ -0,0 +1,9 @@
+﻿import * as Application from './application';
+
+declare var require: (modules: string[], ready: Function, errback: Function) => void;
+
+// Tente de charger du code spécifique à la plateforme à partir du dossier /merges.
+// Plus d'informations sur http://taco.visualstudio.com/fr-fr/docs/configure-app/#Content.
+require(["./platformOverrides"],
+    () => Application.initialize(),
+    () => Application.initialize());
